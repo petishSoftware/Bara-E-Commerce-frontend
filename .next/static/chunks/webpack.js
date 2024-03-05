8fc4dc12f7093f1bbf8ad158435cc524891d30e3
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-03-05 17:17:05 +0200

    chahges

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14c4a3748d5e3dfc"; }
+/******/ 		__webpack_require__.h = function() { return "d348b697d1f7cfc5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
